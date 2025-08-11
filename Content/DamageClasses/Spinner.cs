2e4153b05f70f3665eb26ba2fc124e2a91c3f8dd
Lilac-Wendy <wendygaken@gmail.com>
2025-08-11 18:28:23 -0300

    First Commit: Basic Beyblade

---
+++ Content/DamageClasses/Spinner.cs
@@ -0,0 +1,17 @@
+﻿    using Terraria.ModLoader;
+
+    namespace Gearstorm.Content.DamageClasses
+    {
+        public class Spinner : DamageClass
+        {
+            public override StatInheritanceData GetModifierInheritance(DamageClass damageClass)
+            {
+                return StatInheritanceData.None; // não herda nada
+            }
+
+            public override bool GetEffectInheritance(DamageClass damageClass)
+            {
+                return false;
+            }
+        }
+    }
---
+++ Content/Data/BeybladeStats.cs
@@ -0,0 +1,30 @@
+﻿namespace Gearstorm.Content.Data
+{
+    public struct BeybladeStats(
+        float mass = 0,
+        float density = 0,
+        float height = 0,
+        float radius = 0,
+        float tipFriction = 0,
+        float spinSpeed = 0,
+        float spinDecay = 0,
+        float balance = 0,
+        float knockbackPower = 0,
+        float knockbackResistance = 0,
+        float moveSpeed = 0,
+        float damageBase = 0)
+    {
+        public float Mass = mass;
+        public float Density = density;
+        public float Height = height;
+        public float Radius = radius;
+        public float TipFriction = tipFriction;
+        public float SpinSpeed = spinSpeed;
+        public float SpinDecay = spinDecay;
+        public float Balance = balance;
+        public float KnockbackPower = knockbackPower;
+        public float KnockbackResistance = knockbackResistance;
+        public float MoveSpeed = moveSpeed;
+        public float DamageBase = damageBase;
+    }
+}
